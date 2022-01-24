f7adf63b4cbacc6782356ac20f1587ff7cc68e99
Yan3330 <korolevskijan@gmail.com>
2022-01-24 18:07:11 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,130 @@
+//If-1
+
+// let n = prompt('n=')
+// if (n>0) {
+//     n++
+// }
+// alert(n)
+
+// let n = prompt('n=')
+// if (n>0) {
+//     n++
+// } else {
+//     n = n-2 
+// }
+// alert(`${n}`)
+
+// let n = prompt('n=')
+// if (n>0) {
+//     n++
+// } else if (n<0) {
+//     n = n-2 
+// } else{
+//     n=10
+// }
+// alert(`${n}`)
+
+// let a = prompt('a=')
+// let b = prompt('b=')
+// let c = prompt('c=')
+// let result = 0
+// if (a>0) {
+//     result++
+// }
+// if (b>0) {
+//     result++
+// }
+// if (c>0) {
+//     result++
+// }
+// alert(`Количество полошительных чисел=${result}`)
+
+// let a = prompt('a=')
+// let b = prompt('b=')
+// let c = prompt('c=')
+// let resultplus = 0
+// let resultminus = 0
+// if (a>0) {
+//     resultplus++
+// }
+// if (b>0) {
+//     resultplus++
+// }
+// if (c>0) {
+//     resultplus++
+// }
+// if (a<0) {
+//     resultminus++
+// }
+// if (b<0) {
+//     resultminus++
+// }
+// if (c<0) {
+//     resultminus++
+// }
+// alert(`Количество полошительных чисел=${resultplus}, количество отрицательных чисел=${resultminus}`)
+
+// let a = prompt("Первое число=")
+// let b = prompt("Второе число=")
+// if(a>b) {
+//     alert(a)
+// } 
+// if (a<b) {
+//     alert(b)
+// } 
+// else {
+//     alert(`Эти числа равные`)
+// }
+
+// let a = prompt("Первое число=")
+// let b = prompt("Второе число=")
+// if(a*1>b*1) {
+//     alert(2)
+// } 
+// if (a*1<b*1) {
+//     alert(1)
+// } 
+
+// let a = prompt("Первое число=")
+// let b = prompt("Второе число=")
+// let max = 0
+// let min = 0
+// if(a*1>b*1) {
+//     max = a
+//     min = b
+//     alert(max)
+//     alert(min)
+// } 
+// if (a*1<b*1) {
+//     max = b
+//     min = b
+//     alert(min)
+//     alert(min)
+// } 
+
+// let a = prompt('a=')
+// let b = prompt('b=')
+// a1 = 0
+// b1 = 0
+// if (a>b) {
+//     a1 = b
+//     b1 = a
+//     alert(`a=${a1}, b=${b1}`)
+// } else {
+//     alert(`a=${a}, b=${b}`)
+// }
+
+//10
+
+// let a = prompt('a=')
+// let b = prompt('b=')
+// if (a==b) {
+//     a=0
+//     b=0
+//     alert(`a=${a}, b=${b}`)
+// } else {
+//     a = a*1 + b*1
+//     b = a*1 + b*1
+//     alert(`a=${a}, b=${b}`)
+// }
+
